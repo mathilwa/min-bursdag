8696680b1c792ef8158ed5b07e2081c16d94535e
mathildewarstad <mathildewarstad@bekk.no>
2017-09-22 15:09:07 +0200

    Ikon i stedet for browserknapp

--- app/frontend/App.jsx
+++ app/frontend/App.jsx
@@ -4,14 +4,15 @@ import { polyfill } from 'es6-promise';
 
 import QrReader from 'react-qr-reader';
 import Kamera from './Kamera.jsx';
+import Bla from './Bla.jsx';
 polyfill();
 
 const App = () => {
   return (
     <div>
-      <h1>HEI</h1>
-      <div>
-      </div>
+      <h1>Matta blir 30! </h1>
+      <Bla>
+      </Bla>
       <div>
 
       </div>
--- app/frontend/Bla.jsx
+++ app/frontend/Bla.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const Bla = () => {
+  return (
+    <form>
+      <input id="filopplaster" type="file" multiple accept="image/*" capture="camera"/>
+      <label htmlFor="filopplaster">
+        <i className="fa fa-camera-retro" aria-hidden="true"/>
+      </label>
+    </form>
+  )
+};
+
+export default Bla;
