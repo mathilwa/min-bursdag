0516a5502ea9c5ee7eeb3b6d1bd0d0d0750d7937
mathildewarstad <mathildewarstad@bekk.no>
2017-09-22 14:29:02 +0200

    Oppstart på app

--- app/frontend/App.jsx
+++ app/frontend/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import 'babel-polyfill';
 import { polyfill } from 'es6-promise';
+
+import QrReader from 'react-qr-reader';
+import Kamera from './Kamera.jsx';
 polyfill();
 
 const App = () => {
-  return <h1>HEI</h1>
+  return (
+    <div>
+      <h1>HEI</h1>
+      <div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
 };
 
 export default App;
