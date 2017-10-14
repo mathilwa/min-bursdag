ca4483156f1a199c64b841887aa96e2e3d9942cd
mathildewarstad <mathildewarstad@bekk.no>
2017-10-14 02:02:52 +0200

    siste forsøk for å skjule adressebaren

--- app/frontend/Hovedside.jsx
+++ app/frontend/Hovedside.jsx
@@ -4,28 +4,28 @@ import PropTypes from 'prop-types';
 import Filopplaster from './Filopplaster.jsx';
 import SeAlleBilderKnapp from './SeAlleBilderKnapp.jsx';
 
-// const hideAddressBar = () => {
-//   if(!window.location.hash) {
-//     if(document.height < window.outerHeight) {
-//       document.body.style.height = (window.outerHeight + 50) + 'px';
-//     }
-//
-//     setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-//   }
-// };
+const hideAddressBar = () => {
+  if(!window.location.hash) {
+    if(document.height < window.outerHeight) {
+      document.body.style.height = (window.outerHeight + 50) + 'px';
+    }
+
+    setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+};
 
 class Hovedside extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  // componentDidMount () {
-  //   if (!window.pageYOffset) {
-  //     hideAddressBar();
-  //   }
-  //   window.addEventListener("orientationchange", hideAddressBar );
-  //
-  // }
+  componentDidMount () {
+    if (!window.pageYOffset) {
+      hideAddressBar();
+    }
+    window.addEventListener("orientationchange", hideAddressBar );
+
+  }
 
   render () {
     return (
